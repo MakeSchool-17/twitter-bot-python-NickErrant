01263d748aad8150f53099d1d73928af0301a27e
Nick Munoz-McDonald <nicolas.munoz.mcdonald@students.makeschool.com>
2015-09-29 23:04:44 -0700

    Add histogram using singly linked list

--- linkedlist.py
+++ linkedlist.py
@@ -1,7 +1,31 @@
+class Node():
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def clear(self):
+        link = self.next_node
+        self.next_node = None
+        return link
+
+    def __str__(self):
+        return str(self.data)
+
+
+class AssocNode(Node):
+    def __init__(self, data=None, value=1, next_node=None):
+        super().__init__(data, next_node)
+        self.value = value
+
+    def __str__(self):
+        return str((self.data, self.value))
+
+
 class SortedLinkedList():
-    def __init__(self, iterable=None):
+    def __init__(self, iterable=None, node_type=Node):
         self.head = None
         self.count = 0
+        self.node_type = node_type
         if iterable:
             for item in iterable:
                 self.add(item)
@@ -10,9 +34,16 @@ class SortedLinkedList():
         new_node.next_node = prev_node.next_node
         prev_node.next_node = new_node
 
+    def _find(self, item):
+        current = self.head
+        while current:
+            if current.data == item:
+                return current
+            current = current.next_node
+
     def add(self, item):
         self.count += 1
-        new_node = Node(item)
+        new_node = self.node_type(item)
         current = self.head
         if (not current) or new_node.data < current.data:
             new_node.next_node = current
@@ -61,22 +92,37 @@ class SortedLinkedList():
     def __len__(self):
         return self.count
 
+    def __contains__(self, key):
+        return self._find(key) is not None
 
-class Node():
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
 
-    def clear(self):
-        link = self.next_node
-        self.next_node = None
-        return link
+class HistogramWithList(SortedLinkedList):
+    def __init__(self, iterable=None):
+        super().__init__(iterable, AssocNode)
 
-    def __str__(self):
-        return str(self.data)
+    def add(self, item):
+        node = self._find(item)
+        if node:
+            node.value += 1
+        else:
+            super().add(item)
+
+    def frequency(self, item):
+        node = self._find(item)
+        if node:
+            return node.value
+        return 0
+
+    def subtract(self, item):
+        node = self._find(item)
+        if node:
+            node.value -= 1
+            if node.value < 1:
+                self.remove(item)
 
 
 def main():
+    # I'll write proper unit tests soon...
     ll = SortedLinkedList()
     ll.add(6)
     ll.add(1)
@@ -85,12 +131,31 @@ def main():
     print(ll, len(ll))
     ll.remove("l")
     ll.remove(1)
-    print(ll, len(ll))
+    print(ll, len(ll), 1 in ll, 3 in ll)
     ll.remove(3)
     print(ll)
     ll.remove(6)
     print(ll)
     print(SortedLinkedList(["lol", "a", "gt"]))
+    ll = HistogramWithList()
+    ll.add(6)
+    ll.add(1)
+    ll.add(3)
+    ll.add(2)
+    ll.add(6)
+    print(ll, len(ll))
+    ll.remove("l")
+    ll.remove(1)
+    print(ll, len(ll), 1 in ll, 3 in ll)
+    ll.remove(3)
+    print(ll)
+    ll.subtract(6)
+    print(ll)
+    ll.subtract(6)
+    print(ll)
+    print(ll.frequency(6))
+    print(ll.frequency(2))
+    print(HistogramWithList(["lol", "a", "gt", "lol"]))
 
 if __name__ == '__main__':
     main()
